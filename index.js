9fc1da49de6840088dbf92d9122c05bf6c937664
salah eddin alshaal <salshaal@gmail.com>
2017-05-28 17:38:59 +0300

    test page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var http = require('http');
 var dt = require('./testmodules');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
-    //res.end('Hello Worlds! '+ dt.myDateTime());
+
+    fs.readFile('demotest.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+
 }).listen(8080);
